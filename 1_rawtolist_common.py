5eff97591e089c2bae50485af48e4e493ac7ee82
Tink-Bell <mattosallnat@gmail.com>
2023-09-20 23:46:20 -0700

    cleaned_old_junk

--- 1_rawtolist_common.py
+++ 1_rawtolist_common.py
@@ -1,28 +1,38 @@
 import argparse
 import re
-from collections import Counter
+from collections import Counter, OrderedDict
 import os
 
 def has_vowels(word):
     vowels = "AEIOUYWaeiouyw"
     return any(char in vowels for char in word) and not all(char in vowels for char in word)
 
-def filter_vowelless_lines(input_file, cleaned_output_file):
-    with open(input_file, 'r') as infile, open(cleaned_output_file, 'w') as outfile:
+def filter_vowelless_lines(input_file):
+    cleaned_words = {}
+    with open(input_file, 'r') as infile:
+        line_count = 0
         for line in infile:
+            line_count += 1
             words = line.strip().split()
             filtered_words = [word for word in words if has_vowels(word)]
             if filtered_words:
-                outfile.write(" ".join(filtered_words) + '\n')
+                cleaned_words[line_count] = filtered_words
+    return cleaned_words
 
 def tokenize_text(text):
     words = re.findall(r"\b(?:[A-Za-z]+(?:'\w+)?)\b", text)
     return words
 
-def rank_words(text):
+def rank_words(text, cleaned_words):
     words = tokenize_text(text)
     word_counts = Counter(words)
-    ranked_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    
+    def custom_sort_key(word):
+        frequency = -word_counts[word]
+        alphabetical_order = word
+        return (frequency, alphabetical_order)
+    
+    ranked_words = OrderedDict(sorted(word_counts.items(), key=lambda x: custom_sort_key(x[0])))
     return ranked_words
 
 def main():
@@ -39,19 +49,18 @@ def main():
         print(f"Error: The input file '{args.input_file}' does not exist.")
         return
 
-    cleaned_output_file = "cleaned_" + args.input_file
-    filter_vowelless_lines(args.input_file, cleaned_output_file)
+    cleaned_words = filter_vowelless_lines(args.input_file)
 
-    with open(cleaned_output_file, 'r') as cleaned_file:
-        cleaned_text = cleaned_file.read()
-    
-    ranked_words = rank_words(cleaned_text)
+    with open(args.input_file, 'r') as raw_file:
+        raw_text = raw_file.read()
+
+    ranked_words = rank_words(raw_text, cleaned_words)
 
     with open(args.output_file, 'w') as ranked_file:
-        for word, frequency in ranked_words:
+        for word in ranked_words:
             ranked_file.write(f'{word}\n')
 
-    print(f"Words Cleaned and Ranked . Output saved to '{args.output_file}'")
+    print(f"Words Cleaned and Ranked. Output saved to '{args.output_file}'")
 
 if __name__ == "__main__":
     main()
